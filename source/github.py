0b6acf4038bb22305e46107b80747ed559272c8f
Jussi Vatjus-Anttila <jussiva@gmail.com>
2021-12-12 22:43:06 +0200

    tune grouping functionality and clean pylint warnings

--- source/github.py
+++ source/github.py
@@ -1,18 +1,22 @@
 """ Github log commands """
-import pytest
 from _pytest.terminal import TerminalReporter
 
 
 class Github:
+    """ Github cli command handler """
     def __init__(self, reporter: TerminalReporter):
+        """ Constructor for Github """
         self._reporter = reporter
         # GitHub doesn't support nested grouping so this ensures we do not have any.
         self._group_activated = False
 
     def gh_print(self, command: str, value: str = "") -> None:
         """ Github command prints"""
+        self.write_line(f'::{command}::{value}')
+
+    def write_line(self, data: str):
         if self._reporter:
-            self._reporter.write_line(f'::{command}::{value}', flush=True)
+            self._reporter.write_line(data, flush=True)
 
     def start_github_group(self, name: str, prefix="", postfix="") -> None:
         """
--- source/plugin.py
+++ source/plugin.py
@@ -2,7 +2,6 @@
 Pytest plugin for github action log grouping.
 """
 import os
-from collections import Callable
 import pytest
 from _pytest.config import _strtobool
 from _pytest.config import Config
@@ -16,22 +15,26 @@ TERMINAL_REPORT = None
 
 
 def pytest_addoption(parser: Parser) -> None:
+    """ Initialize argument parsing. """
     group = parser.getgroup("pytest_gh_log_group")
     group.addoption(
         "--gh_log_group",
         action="store_true",
         dest="gh_log_group",
         default=_strtobool(os.getenv('GITHUB_ACTIONS')),
-        help="Time in milliseconds when the print was invoked, relative to the time the fixture was created.",
+        help="Time in milliseconds when the print was invoked, "
+             "relative to the time the fixture was created.",
     )
 
 
 @pytest.hookimpl(trylast=True)
 def pytest_configure(config: Config):
+    """ Initilize Github command printer """
     pytest.grouping_session = create(config)
 
 
 def create(config: Config) -> Github:
+    """ Create object """
     if config.getoption("gh_log_group"):
         terminal_reporter = config.pluginmanager.getplugin("terminalreporter")
         return Github(reporter=terminal_reporter)
@@ -40,11 +43,13 @@ def create(config: Config) -> Github:
 
 @pytest.hookimpl(tryfirst=True)
 def pytest_collection(session: Session):  # pylint: disable=unused-argument
+    """ Start files/folders collection"""
     pytest.grouping_session.start_github_group('collector')
 
 
 @pytest.hookimpl(trylast=True)
 def pytest_collection_finish(session):  # pylint: disable=unused-argument
+    """ Collection finish """
     pytest.grouping_session.end_github_group()
 
 
@@ -70,11 +75,12 @@ def pytest_runtest_setup(item) -> None:
     pytest.grouping_session.start_github_group(item.name, prefix="TEST", postfix="SETUP")
 
 
-@pytest.hookimpl(trylast=True)
+@pytest.hookimpl(tryfirst=True)
 def pytest_runtest_teardown(item) -> None:
     """
     Start group "TEST TestName TEARDOWN"
     """
+    pytest.grouping_session.write_line('')
     pytest.grouping_session.start_github_group(item.name, prefix="TEST", postfix="TEARDOWN")
 
 
@@ -104,4 +110,3 @@ def pytest_fixture_setup(request, fixturedef) -> None:
     yield
 
     pytest.grouping_session.end_github_group()
-
