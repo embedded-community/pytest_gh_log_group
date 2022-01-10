f2e783d10e79122517511acc7b42c86df3ad3f5b
artodrees <88309388+artodrees@users.noreply.github.com>
2022-01-10 13:40:20 +0200

    parameter tests

--- tests/test_e2e.py
+++ tests/test_e2e.py
@@ -15,22 +15,32 @@ def test_setup():
     print('after test_setup')
 
     
-@pytest.fixture(scope='function', params=[])
+@pytest.fixture(scope='function', params=[], ids=['p1', 'p2'])
 def test_setup_with_parameters(request):
     print(f'test_setup_with_parameters {request.param}')
     yield
     print('after test_setup_with_parameters')
-    
+
 
 def test_sample1(session_setup, test_setup):
     print('test#1')
 
+
 def test_sample2(session_setup, test_setup):
     print('test#2')
 
-@pytest.mark.parametrize("test_setup_with_parameters", argvalues=[{'param1': 1.1, 'param2': 'parameter2 from test'}], indirect=True)
-def test_sample3(session_setup, test_setup_with_parameters):
-    print('test#3')
+
+@pytest.mark.parametrize("param1",
+                         argvalues=[1.1,])
+def test_test_parameter(session_setup, param1):
+    print('test#3 test_fixture_parameter')
+
+
+@pytest.mark.parametrize("test_setup_with_parameters",
+                         argvalues=[{'param1': 1.1, 'param2': 'parameter2 from test'}],
+                         indirect=True, ids=['params'])
+def test_fixture_parameter(session_setup, test_setup_with_parameters):
+    print('test#4 test_fixture_parameter')
 
 
 def test_fail():
