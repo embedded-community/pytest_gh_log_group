d1502c910902cc0a621b765fcf174eba5e58e6dd
artodrees <88309388+artodrees@users.noreply.github.com>
2022-01-10 13:42:22 +0200

    ensure command is on new line

--- source/github.py
+++ source/github.py
@@ -7,6 +7,7 @@ class Github:
     def __init__(self, reporter: TerminalReporter):
         """ Constructor for Github """
         self._reporter = reporter
+
         # GitHub doesn't support nested grouping so this ensures we do not have any.
         self._active_group = None
 
@@ -17,13 +18,14 @@ class Github:
     def write_line(self, data: str):
         """ write line using reporter """
         if self._reporter:
-            self._reporter.write_line(data, flush=True)
+            self._reporter.write_ensure_prefix(prefix='', extra=data, flush=True)
+
 
     def start_github_group(self, name: str, prefix="", postfix="") -> None:
         """
         Starts a log group in Github Actions Log
         """
-        self.end_github_group()
+        self.end_github_group()  # GitHub doesn't support nested grouping
         value = (prefix + " " + name + " " + postfix).strip(" ")
         self.write_command('group', value)
         self._active_group = value
