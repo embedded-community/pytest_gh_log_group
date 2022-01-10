bd9eead91cfd0ac32047bf92eb96c57b007b61cf
artodrees <88309388+artodrees@users.noreply.github.com>
2022-01-10 15:17:35 +0200

    don't add newlines after commands

--- source/github.py
+++ source/github.py
@@ -18,8 +18,6 @@ class Github:
     def write_line(self, data: str):
         """ write line using reporter """
         if self._reporter:
-            if data and data[-1] is not '\n':
-                data += '\n'
             self._reporter.write_ensure_prefix(prefix='', extra=data, flush=True)
 
 
